c2c0df998771675190a20be8c5ca9f1e0fae4142
QuCM <q.martincocher@gmail.com>
2021-10-13 19:45:08 +0200

    Fix data import as well as creation if one already exists

--- import_reseau_cyclable.py
+++ import_reseau_cyclable.py
@@ -6,10 +6,12 @@ PASSWORD = "password"
 
 couch = couchdb.Server()  
 couch.resource.credentials = (USERNAME, PASSWORD)
+
+couch.delete('reseau-cyclable')
 db = couch.create('reseau-cyclable')  
 
-with open('reseau-cyclable-et-vert.json') as f:
+with open('data/reseau-cyclable-et-vert.json') as f:
   data = json.load(f)
 
 for element in data:
-    db.save(element['fields'])
+    db.save(element)
