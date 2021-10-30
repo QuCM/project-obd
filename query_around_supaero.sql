32bc2e049ea5ec58b3ad1fce0d464a7b15924a6c
QuCM <q.martincocher@gmail.com>
2021-10-30 18:03:45 +0200

    Fix distance calculation

--- query_around_supaero.sql
+++ query_around_supaero.sql
@@ -3,18 +3,24 @@
 CREATE OR REPLACE FUNCTION is_near(coordinates POINT[], place POINT, epsilon FLOAT)
     RETURNS BOOLEAN LANGUAGE plpgsql AS $$
 
+    DECLARE
+        DEG_TO_RAD FLOAT;
+
     BEGIN
         IF CARDINALITY(coordinates) IS NULL THEN
             RETURN false;
         END IF;
         
+        DEG_TO_RAD := PI()/180;
+
         FOR i IN 0..CARDINALITY(coordinates) LOOP
-            IF ACOS(SIN((coordinates[i])[1]) * SIN(place[1]) + COS((coordinates[i])[1]) * COS(place[1]) * COS((coordinates[i])[0] - place[0])) * 6378137 < POWER(epsilon, 2) THEN
+            IF ACOS(SIN((coordinates[i])[1]*DEG_TO_RAD) * SIN(place[1]*DEG_TO_RAD) + COS((coordinates[i])[1]*DEG_TO_RAD) * COS(place[1]*DEG_TO_RAD) * COS((coordinates[i])[0]*DEG_TO_RAD - place[0]*DEG_TO_RAD)) * 6378137 < epsilon THEN
                 RETURN true;
             END IF;
         END LOOP;
         RETURN false;
-    END $$;
+    END $$
+;
 
 
 SELECT 
@@ -24,4 +30,5 @@ FROM
 WHERE 
     is_near(geo_shape_coordinates_1, '(1.471564, 43.570297)'::POINT, 100) OR
     is_near(geo_shape_coordinates_2, '(1.471564, 43.570297)'::POINT, 100) OR
-    is_near(geo_shape_coordinates_3, '(1.471564, 43.570297)'::POINT, 100);
+    is_near(geo_shape_coordinates_3, '(1.471564, 43.570297)'::POINT, 100)
+;
